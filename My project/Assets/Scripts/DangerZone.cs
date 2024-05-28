ff626d1e3f2171be280f1414158497a216c3acfe
Paullleta <davids.garciam@utadeo.edu.co>
2024-05-28 12:08:09 -0500

    ZonaPeligrosa
    Ya esta que el cubo detecte cuando pierde

---
+++ My project/Assets/Scripts/DangerZone.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class DetectarSalidaZona : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Block"))
+        {
+            Debug.Log("¡Has perdido! Un bloque ha entrado en la zona no segura.");
+            Perder();
+        }
+    }
+
+    private void Perder()
+    {
+        Debug.Log("Juego Terminado");
+        // Aquí puedes agregar la lógica para finalizar el juego, como reiniciar el nivel
+        // o mostrar una pantalla de Game Over.
+    }
+}
